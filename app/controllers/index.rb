251f865c017c5b8e3ea081a69f13e123ff6abeee
David Keller <dckeller1@gmail.com>
2018-01-21 11:20:31 -0700

    Intialized database

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,18 @@
+# Look in app/views/index.erb
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+	puts "[LOG] Responding to a GET request for /"
+	puts "[LOG] Params: #{params.inspect}"
+	erb :index
 end
+
+get '/sanbox' do
+	puts "[LOG] Responding to a GET request for /sandbox"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :get_sandbox
+end
+
+post '/sandbox' do
+	puts "[LOG] Responding to a POST request to /sandbox"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :post_sandbox
+end 
