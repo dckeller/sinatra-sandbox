3ebc245847b01485f981d4668db48251b01bcac7
David Keller <dckeller1@gmail.com>
2018-01-24 13:25:10 -0700

    Complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,13 +6,14 @@ get '/' do
 end
 
 get '/sandbox' do
-	@users = User.all
+	@user = params[:post]
 	puts "[LOG] Responding to a GET request for /sandbox"
   puts "[LOG] Params: #{params.inspect}"
   erb :get_sandbox
 end
 
 post '/sandbox' do
+	@user = params[:post]
 	puts "[LOG] Responding to a POST request to /sandbox"
   puts "[LOG] Params: #{params.inspect}"
   erb :post_sandbox
